5093bcefe75527517a8a837e9d174f0a2e5e01c7
ConanWu <457907449@qq.com>
2018-11-06 00:12:09 +0800

    @property with xlrd package python_subtest separate code and data(put data in excel file)

--- Test_Framework/test/case/test_baidu.py
+++ Test_Framework/test/case/test_baidu.py
@@ -5,27 +5,29 @@ import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from ...utils.config import Config, DRIVER_PATH
+from ...utils.config import Config, DRIVER_PATH, DATA_PATH
 from ...utils.log import logger
+from ...utils.file_reader import ExcelReader
 
 
 class TestBaidu(unittest.TestCase):
     URL = Config().get('URL')
+    excel = DATA_PATH + '/baidu百度.xlsx'
 
     locator_kw = (By.ID, 'kw')
     locator_su = (By.ID, 'su')
     locator_result = (By.XPATH, '//div[contains(@class, "result")]/h3/a')
 
-    def setUp(self):
+    def sub_setUp(self):
         option = webdriver.ChromeOptions()
         option.add_argument("headless")
         self.driver = webdriver.Chrome(executable_path=DRIVER_PATH + '\chromedriver.exe', chrome_options=option)
         self.driver.get(self.URL)
 
-    def tearDown(self):
+    def sub_tearDown(self):
         self.driver.quit()
 
-    def test_search_0(self):
+    def te1st_search_0(self):
         self.driver.find_element(*self.locator_kw).send_keys('selenium')
         self.driver.find_element(*self.locator_su).click()
         time.sleep(2)
@@ -34,7 +36,7 @@ class TestBaidu(unittest.TestCase):
             print(link.text)
             logger.info(link.text)
 
-    def test_search_1(self):
+    def te1st_search_1(self):
         self.driver.find_element(*self.locator_kw).send_keys('250')
         self.driver.find_element(*self.locator_su).click()
         time.sleep(2)
@@ -42,6 +44,20 @@ class TestBaidu(unittest.TestCase):
         for link in links:
             print(link.text)
             logger.info(link.text)
+
+    def test_search_excel(self):
+        datas = ExcelReader(self.excel).data
+        for d in datas:
+            with self.subTest(data=d):
+                self.sub_setUp()
+                self.driver.find_element(*self.locator_kw).send_keys(d['search'])
+                self.driver.find_element(*self.locator_su).click()
+                time.sleep(2)
+                links = self.driver.find_elements(*self.locator_result)
+                for link in links:
+                    print(link.text)
+                    logger.info(link.text)
+                self.sub_tearDown()
 
 
 if __name__ == '__main__':
